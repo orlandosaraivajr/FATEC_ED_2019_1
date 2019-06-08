b2f7ad823c0a522a60fe18f866ebf10c744d30c2
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2019-06-08 08:42:34 -0300

    Add files via upload

---
+++ GravaDisco/main.cpp
@@ -0,0 +1,37 @@
+// basic file operations
+/*
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main () {
+  ofstream myfile;
+  myfile.open ("example.txt");
+  myfile << "Writing this to a file.\n";
+  myfile.close();
+  return 0;
+}
+*/
+// reading a text file
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main () {
+  string line;
+  ifstream myfile ("example.txt");
+  if (myfile.is_open())
+  {
+    while ( getline (myfile,line) )
+    {
+      cout << line << '\n';
+    }
+    myfile.close();
+  }
+
+  else cout << "Unable to open file"; 
+
+  return 0;
+}
+
